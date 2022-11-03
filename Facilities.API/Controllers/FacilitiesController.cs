87926e4bcd999479247b988e4af26c5d8d6f95cf
Isaac Ikusika <helloayodeji@gmail.com>
2022-11-03 11:15:25 +0100

    Add project files.

---
+++ Facilities.API/Controllers/FacilitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Facilities.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FacilitiesController : ControllerBase
+    {
+        private readonly ILogger<FacilitiesController> _logger;
+
+        public FacilitiesController(ILogger<FacilitiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        public record FacilityInfo(long Patient_Id, string Name, DateTimeOffset Date_Created, bool Submissions);
+    }
+}
